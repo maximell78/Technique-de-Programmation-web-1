f0c78442b326aa0655dfcced39fdee041f8cc717
maximell78 <74106736+maximell78@users.noreply.github.com>
2020-11-18 23:36:30 -0500

    Premier Commit
    Envoie de tout les fichiers sur GitHub.

---
+++ Projet/MonSiteWeb/js/dynamic.js
@@ -0,0 +1,8 @@
+function valeurDeMaVariable(num1){
+                       
+    if (num1 == 5) {
+       alert("La variable vaut 5");
+    }else{
+        alert("La variable ne vaut pas 5");
+    }  
+}
